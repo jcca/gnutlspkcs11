12f56e0ca8a5c765fbc904a2f8eef4907b60784a
jcca <jccarlos.a@gmail.com>
2017-07-04 12:58:45 -0400

    sign pdf test

--- src/test/java/org/gnutlspkcs11/P11SignatureTest.java
+++ src/test/java/org/gnutlspkcs11/P11SignatureTest.java
@@ -18,15 +18,24 @@
 
 package org.gnutlspkcs11;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.*;
+import com.itextpdf.text.pdf.security.*;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.*;
 
+import java.io.*;
 import java.security.PrivateKey;
+import java.security.Provider;
 import java.security.PublicKey;
 import java.security.Security;
 import java.security.Signature;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by jcca on 6/21/17.
@@ -40,6 +49,19 @@ class P11SignatureTest {
     @BeforeAll
     public static void before() {
         Security.addProvider(new GnutlsPKCS11());
+    }
+
+    public static byte[] emptyPDF() throws DocumentException, IOException {
+        Document document = new Document();
+        OutputStream baosPDF = new ByteArrayOutputStream();
+        PdfWriter.getInstance(document, baosPDF);
+        document.open();
+        document.add(new Paragraph("Hello, world!"));
+        document.close();
+        byte[] result = ((ByteArrayOutputStream) baosPDF).toByteArray();
+        baosPDF.close();
+
+        return result;
     }
 
     @Test
@@ -56,9 +78,50 @@ class P11SignatureTest {
         Assertions.assertTrue(publicSignature.verify(signature));
     }
 
+    @Disabled("Run only if you have a real pkcs11 token.")
     @Test
     void signPDF() throws Exception {
+        InputStream certstream = new FileInputStream("<CERTIFICATE>");
+        CertificateFactory cf = CertificateFactory.getInstance("X.509");
+        Certificate crt = cf.generateCertificate(certstream);
+        Certificate[] chain = new Certificate[] {crt};
+        OutputStream os = new ByteArrayOutputStream();
 
+        Provider p = new GnutlsPKCS11();
+        Security.addProvider(p);
+
+        PKCS11 p11 = new PKCS11();
+
+        PrivateKey pk = p11.loadPrivateKey(URL);
+
+        byte [] pdf = emptyPDF();
+        PdfReader reader = new PdfReader(pdf);
+        PdfStamper stamper = PdfStamper.createSignature(reader, os, '\0');
+        PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+        appearance.setReason("I'm agree");
+        ExternalSignature pks = new PrivateKeySignature(pk, DigestAlgorithms.SHA256, p.getName());
+        ExternalDigest digest = new BouncyCastleDigest();
+        MakeSignature.signDetached(appearance, digest, pks, chain, null, null, null, 0, MakeSignature.CryptoStandard.CMS);
+
+        reader.close();
+        os.flush();
+        os.close();
+
+        p = new BouncyCastleProvider();
+        Security.addProvider(p);
+        reader = new PdfReader(((ByteArrayOutputStream) os).toByteArray());
+        AcroFields af = reader.getAcroFields();
+        List<String> names = af.getSignatureNames();
+        for (String name : names) {
+            System.out.println("Signature name: " + name);
+            System.out.println("Signature covers whole document: " + af.signatureCoversWholeDocument(name));
+            System.out.println("Document revision: " + af.getRevision(name) + " of " + af.getTotalRevisions());
+            PdfPKCS7 p7 = af.verifySignature(name);
+            Calendar cal = p7.getSignDate();
+            //Certificate[] pkc = pk.getCertificates();
+            System.out.println("Subject: " + CertificateInfo.getSubjectFields(p7.getSigningCertificate()));
+            System.out.println("Revision modified: " + !p7.verify());
+        }
     }
 
     @AfterAll
